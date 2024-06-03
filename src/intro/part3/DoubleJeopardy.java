e9fbc3981a662e4eb98a541e59576a838124a909
GitHalex <calex1731@gmail.com>
2024-06-02 21:56:21 -0400

    nuevo variabel agregado

--- src/intro/part3/DoubleJeopardy.java
+++ src/intro/part3/DoubleJeopardy.java
@@ -3,6 +3,8 @@ package intro.part3;
 public class DoubleJeopardy {
     public static void main(String[] args) {
         double value = 32;
+        int valor2 = 132;
         System.out.println("A double: " + value);
+        System.out.println("A double: " + valor2);
     }
 }
